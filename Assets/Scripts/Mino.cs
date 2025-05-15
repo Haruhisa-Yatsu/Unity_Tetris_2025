b3b6e15677a1625cd2a7476fb4fce316472cb423
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-15 12:21:56 +0900

    Minoスクリプトの作成　05/15終了時点

---
+++ Assets/Scripts/Mino.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mino : MonoBehaviour
+{
+    /// <summary>
+    /// 位置縦
+    /// </summary>
+    private int _posY;
+
+    /// <summary>
+    /// 位置横
+    /// </summary>
+    private int _posX;
+
+    /// <summary>
+    /// 落下のカウンタ
+    /// </summary>
+    private float _fallCount;
+    
+    /// <summary>
+    /// 落下の秒数
+    /// </summary>
+    private float _fallSecond;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
