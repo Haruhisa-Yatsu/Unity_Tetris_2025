c3ece77e5d31d07d659e5f596b6fcf1e2841c124
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-16 10:50:33 +0900

    ミノの移動処理

--- Assets/Scripts/Mino.cs
+++ Assets/Scripts/Mino.cs
@@ -18,22 +18,86 @@ public class Mino : MonoBehaviour
     /// 落下のカウンタ
     /// </summary>
     private float _fallCount;
-    
+
     /// <summary>
     /// 落下の秒数
     /// </summary>
+    [SerializeField]
     private float _fallSecond;
 
+    /// <summary>
+    /// 2D用のTransform
+    /// </summary>
+    private RectTransform _rect;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _posX = 4;
+        _posY = 19;
+
+        _fallCount = 0.0f;
+
+        _rect = transform as RectTransform;
+
+        PositionUpdate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Right();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Left();
+        }
+
+
+
+        _fallCount += Time.deltaTime;
+
+        if (_fallCount > _fallSecond)
+        {
+            Fall();
+            _fallCount -= _fallSecond;
+        }
+    }
+
+    /// <summary>
+    /// 落下処理
+    /// </summary>
+    private void Fall()
+    {
+        _posY -= 1;
+        PositionUpdate();
+    }
+
+    /// <summary>
+    /// 右移動
+    /// </summary>
+    private void Right()
+    {
+        _posX += 1;
+        PositionUpdate();
+    }
+
+    /// <summary>
+    /// 右移動
+    /// </summary>
+    private void Left()
+    {
+        _posX -= 1;
+        PositionUpdate();
+    }
+
+    /// <summary>
+    /// ミノの位置を更新
+    /// </summary>
+    private void PositionUpdate()
+    {
+        _rect.localPosition = new Vector3(_posX, _posY, 0);
     }
 }
