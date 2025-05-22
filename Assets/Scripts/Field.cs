bd4b03bb55dce6a4df8d96a824820a83bc0d4b44
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-22 10:22:51 +0900

    Minoのプレハブ化 FieldクラスからMinoプレハブの生成をするように修正

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -7,11 +7,19 @@ public class Field : MonoBehaviour
     public const int WIDTH = 10;
     public const int HEIGHT = 20;
 
+    /// <summary>
+    /// Minoのプレハブ
+    /// </summary>
+    [SerializeField]
+    private Mino _minoPrefab;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ミノの生成処理
+        // Fieldの子として生成する
+        Instantiate(_minoPrefab, transform);
     }
 
     // Update is called once per frame
