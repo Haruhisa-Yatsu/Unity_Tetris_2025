a905fe349e60e4a70acee97667584ff83b6a722d
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-22 11:43:34 +0900

    FieldBlockを盤面上すべての場所に生成

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -13,6 +13,12 @@ public class Field : MonoBehaviour
     [SerializeField]
     private Mino _minoPrefab;
 
+    /// <summary>
+    /// FieldBlockのプレハブ
+    /// </summary>
+    [SerializeField]
+    private FieldBlock _fieldBlockPrefab;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +26,30 @@ public class Field : MonoBehaviour
         // ミノの生成処理
         // Fieldの子として生成する
         Instantiate(_minoPrefab, transform);
+
+        // 盤面の黒部分すべてに
+        // _fieldBlockPrefabを敷き詰める処理を記述せよ
+
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+                // Instantiateの戻り値を保管
+                FieldBlock fieldBlock = Instantiate(_fieldBlockPrefab, transform);
+                
+                //　保管したFieldBlockからRectTransformを取得
+                RectTransform rect = fieldBlock.transform as RectTransform;
+                
+                // ローカルポジションを更新
+                rect.localPosition = new Vector3(j, i, 0);
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
