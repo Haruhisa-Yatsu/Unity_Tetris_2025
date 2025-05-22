73b8288e548d81f7ea9c5933006d273ab51fd90f
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-05-22 10:56:41 +0900

    FieldBlockの作成

---
+++ Assets/Scripts/FieldBlock.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Imageクラスを使用するのに必要
+using UnityEngine.UI;
+
+public class FieldBlock : MonoBehaviour
+{
+    // 画像の表示 → Image
+    // Imageの色を指定できるようにする
+    // 表示非表示の切り替え
+
+    /// <summary>
+    /// Imageコンポーネント
+    /// </summary>
+    [SerializeField]
+    private Image _image;
+
+    /// <summary>
+    /// 表示非表示の切り替え
+    /// </summary>
+    public void SetEnable(bool enable)
+    {
+        _image.enabled = enable;
+    }
+
+    /// <summary>
+    /// Imageの色を設定する
+    /// </summary>
+    /// <param name="color"></param>
+    public void SetColor(Color color)
+    {
+        _image.color = color;
+    }
+
+}
